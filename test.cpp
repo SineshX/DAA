9ac50a06edfc617a32e4c226403598eda40bd1ac
Sinesh <sineshkumarr@gmail.com>
2021-04-02 18:37:55 +0530

    added gitignore

--- test.cpp
+++ test.cpp
@@ -6,6 +6,7 @@ int main()
 {
     
     cout<<"Hello World \n";
+    cout<<" Till I Collapese  , ah , some timees yoou feel waek you gotta find that inner strenght til ii collapse the i drop when i am not im not just eminem corpse silly i am not the criminal the seap 2paac sprit do you hear the pop listen up till the light till muy leg up i am high thill the bone comes cant shut my mouthh i am under magic music is like magic try as mich here the list so i am not emillie you hated it but til the ske till the wound til iam under as soon the verse start how peoplle everse ythe death of reverse my thoughts frag like kil pin contant battle this is like fillblown the fact the platinum status till the i m the rip the sit thill my leg give out i with the siti untill the wound i cannt feel my kegt i will not fALL I WILL stand up                      ";
         
     return 0;
 }
