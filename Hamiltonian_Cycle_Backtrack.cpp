9608c61d542f969dc62951cc870228c933bd553b
Sinesh <sineshkumarr@gmail.com>
2021-06-11 07:43:23 +0530

    .

--- Hamiltonian_Cycle_Backtrack.cpp
+++ Hamiltonian_Cycle_Backtrack.cpp
@@ -1,7 +1,92 @@
-#include "iostream"
+/*
+ ****************************************
+ * Name   :   Sinesh Kumar
+ * Roll No:   1917286
+ * Date   :   20-04-2021
+ ****************************************
+ * Problem:  Find All Hamiltonian Cycle 
+ ****************************************
+ *	visit all nodes exactly once and come back to origin
+ * find all possible cycles
+ ****************************************
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int graph[5][5] = {{0, 1, 0, 1, 0},
+                   {1, 0, 1, 1, 1},
+                   {0, 1, 0, 0, 1},
+                   {1, 1, 0, 0, 0},
+                   {0, 1, 1, 0, 0}};
+
+int n = sizeof(graph) / sizeof(graph[0]);
+// 100/20 , coz 25*4 = 100 and 5*4 = 20 // size of int = 4
+
+vector<int> x(n, 0); // init all with 0
+// for storing cycle node values :p not index
+
+void hamiltonian(int k);
+void nextVertex(int k);
+//defined after main function
 
 int main()
 {
+    for (int i : x)
+    {
+        cout << i << " ";
+    }
+    int source = 1; //1 is source vertex
+    x.at(0) = source;
+    cout << x.at(0);
 
+    //k = 1 , is index of x vector (stores cycle nodes)
+    // intitially x[k] = 0 // coz initilized
+    hamiltonian(1);
     return 0;
+} //end of main fun
+
+void hamiltonian(int k)
+{
+    do
+    {
+        nextVertex(k);
+
+        break;
+
+    } while (true);
 }
+
+void nextVertex(int k)
+{   
+    do
+    {
+        x[k] = (x[k] + 1) % (n + 1); // n = 5
+        //1 2 3 4 5 0 -- again 1 2 3 4 5 0 --etc
+        if (x[k] == 0)
+        {
+            return;
+        } //if found zero
+
+        //first run me x[1] = 1
+
+        //now checking if edge exist
+        if (graph[x[k - 1] - 1][x[k] - 1] != 0) //extra -1 coz graph is frim 0
+        {   //since edge exist ___
+            int j;
+            for (j = 0; j <= k; j++)        
+            {  //  checking for duplicate entry in x vector till filled ( k-1 index tak)
+                if (x[j] == x[k])
+                {
+                    break;
+                }
+            }//end of loop
+            if (j == k)
+
+        }
+
+        break; //to be removed
+    } while (true);
+}
+
--- vector.cpp
+++ vector.cpp
@@ -4,10 +4,36 @@ using namespace std;
 //dont forget this, annoyed me for 1 fking hr ;
 
 int main()
-{
-    vector<int> v;
+{   int size =10;
+    vector<int> v1(size,0);
+    //init all 10 values with 0
+    for(int x:v1)
+    {
+        cout<<x<<" ";
+    }
+    // vector<int> v[4];  // u can't
 
-    v.push_back(5);
-    cout<<v[0]<<endl;
+
+    vector<int> v2;
+    int x2;
+    // v.push_back(5);
+    cout<<endl<<sizeof(v2); //12 why ??
+    cout<<endl<<sizeof(x2); //4
+
+    vector<int> v3 {1,2,3,6,7,8};
+    cout<<endl<<v3[5]; //access successful :)
+
+    int size_2 = 20 ;
+    vector<int> v4(size_2); //x3 is size
+    int value = 8;
+    fill(v4.begin(),v4.end(),value);
+    for(int i : v4) { cout<<i <<" "; }
+
+    cout<<endl;
+    
+    vector<int> v5(2);
+    v5[0] = 20;
+    cout<<v5[0]<<endl;
+
     return 0;
 }
